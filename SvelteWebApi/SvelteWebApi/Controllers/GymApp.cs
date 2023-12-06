038fd162cfc204abd7a236872a33ab1f57cf282d
lemon-sauce <harveyemmerson122004@gmail.com>
2023-12-06 21:17:25 +0000

    added project files

---
+++ SvelteWebApi/SvelteWebApi/Controllers/GymApp.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GymApp : ControllerBase
+    {
+        private IConfiguration _configuration;
+
+        public GymApp(IConfiguration pConfiguration)
+        {
+            _configuration = pConfiguration;
+        }
+
+        [HttpGet]
+        [Route("GetCredentials")]
+
+        public JsonResult GetCredentials(string pEmail, string pPassword)
+        {
+            string isValid = "";
+            string query = $"SELECT Email,UserPassword FROM users WHERE Email = '{pEmail}' AND UserPassword = '{pPassword}'";
+            string SqlDataSource = _configuration.GetConnectionString("GymAppDBcon");
+            MySqlDataReader reader;
+
+            using (MySqlConnection connection = new MySqlConnection(SqlDataSource))
+            {
+                connection.Open();
+                using (MySqlCommand command = new MySqlCommand(query, connection))
+                {
+                    reader = command.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        isValid = "true";
+                    }
+                    else
+                    {
+                        isValid = "false";
+                    }
+                    reader.Close();
+                    connection.Close();
+                }
+            }
+            return new JsonResult(isValid);
+        }
+    }
+}
---
+++ WebUI/UI/src/header.svelte
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-light">
+	<div class="container">
+		<a class="navbar-brand" href="https://github.com/harvey4452">My github</a>
+	</div>
+</nav>
+
+<style>
+	a{
+		text-decoration:none;
+		color:blue;
+		font-size:2.5em;
+		font-weight:900;
+	}
+	
+</style>
+
+<script>
+
+</script>
