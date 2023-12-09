c4f66b9870025d739c7b3e03fd9fe5a14ad5c0f8
lemon-sauce <harveyemmerson122004@gmail.com>
2023-12-09 00:48:21 +0000

    Delete function connected to frontend, admin can now create and delete accounts

--- WebUI/UI/src/AdminHomepage.svelte
+++ WebUI/UI/src/AdminHomepage.svelte
@@ -6,6 +6,9 @@
     let Lname;
     let AccessLevel;
 
+    let DeleteEmail;
+    let displayDeleteMessage = 'false';
+
     let API_URL = "http://localhost:5035/";
 
     function CreateAccount(){
@@ -22,39 +25,72 @@
 		});
     }
 
+    function DeleteAccount(){
+        let newUrl = API_URL+"api/GymApp/DeleteAccount?pEmail="+DeleteEmail;
+        fetch(newUrl,{method:"DELETE"})
+		.then((response)=>response.json())
+		.then((data)=>{
+			if (data == "Account deleted!"){
+                displayDeleteMessage = 'success';
+            }
+            else if(data == "No account with that email exists!"){
+                displayDeleteMessage = 'error';
+            }
+		});
+    }
+
 
 </script>
 
 <div class="CreateAccountContainer">
     <div class="row">
-        <div class="col align-self-center">
-            <input class="inputBox" bind:value={Email} placeholder="Email" />
+        <div class="column">
+            <div class="align-self-center">
+                <input class="inputBox" bind:value={Email} placeholder="Email" />
+            </div>
+            <div class="align-self-center">
+                <input class="inputBox" bind:value={Password} placeholder="Password" />
+            </div>
+            <div class="align-self-center">
+                <input class="inputBox" bind:value={Fname} placeholder="Forename" />
+            </div>
+            <div class="align-self-center">
+                <input class="inputBox" bind:value={Lname} placeholder="Surname" />
+            </div>
+            <div class="align-self-center">
+                <input class="inputBox" bind:value={AccessLevel} placeholder="Access level" />
+            </div>
+            {#if displayMessage == 'error'}
+                <p class="ErrorMessage">Account with email already exists in our database!</p>
+            {:else if displayMessage == 'success'}
+                <p class="SuccessMessage">Account created!</p>
+            {/if}
+            <div class="align-self-right">
+                <button class="buttonStyle" on:click={CreateAccount}>Create new account</button>
+            </div>
         </div>
-        <div class="col align-self-center">
-            <input class="inputBox" bind:value={Password} placeholder="Password" />
-        </div>
-        <div class="col align-self-center">
-            <input class="inputBox" bind:value={Fname} placeholder="Forename" />
-        </div>
-        <div class="col align-self-center">
-            <input class="inputBox" bind:value={Lname} placeholder="Surname" />
-        </div>
-        <div class="col align-self-center">
-            <input class="inputBox" bind:value={AccessLevel} placeholder="Access level" />
-        </div>
-        {#if displayMessage == 'error'}
-            <p class="ErrorMessage">Account with email already exists in our database!</p>
-        {:else if displayMessage == 'success'}
-            <p class="SuccessMessage">Account created!</p>
-        {/if}
-        <div class="col align-self-right">
-            <button class="buttonStyle" on:click={CreateAccount}>Create new account</button>
+        <div class="column">
+            <div class="align-self-center">
+                <input class="inputBox" bind:value={DeleteEmail} placeholder="Email" />
+            </div>
+            {#if displayDeleteMessage == 'error'}
+                <p class="ErrorMessage">Account with email does not exist in our database!</p>
+            {:else if displayDeleteMessage == 'success'}
+                <p class="SuccessMessage">Account deleted!</p>
+            {/if}
+            <div class="align-self-right">
+                <button class="buttonStyle" on:click={DeleteAccount}>Delete account</button>
+            </div>
         </div>
     </div>
 </div>
-<p>Admin homepage</p>
 
 <style>
+    .column{
+        float: left;
+        width: 50%;
+    }
+
 	.ErrorMessage{
         color:red;
     }
